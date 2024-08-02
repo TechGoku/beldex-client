671acfa4779f4cf7bee642aeaaf1c775b40cc598
Sanada  Yukimura <45681066+sanada08@users.noreply.github.com>
2024-08-02 08:15:00 +0530

    Update archive-emcpp-dev.sh

--- bin/archive-emcpp-dev.sh
+++ bin/archive-emcpp-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 bin/build-emcpp-dev.sh &&
-cp build/MyMoneroClient_WASM.js src/; 
-cp build/MyMoneroClient_WASM.wasm src/;
-cp build/MyMoneroClient_WASM.wasm.map src/;
+cp build/BeldexClient_WASM.js src/; 
+cp build/BeldexClient_WASM.wasm src/;
+cp build/BeldexClient_WASM.wasm.map src/;
