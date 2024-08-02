b2b4039a0512862eac33b732d7d47d50b839dc70
Sanada  Yukimura <45681066+sanada08@users.noreply.github.com>
2024-08-02 08:17:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const WABridge = require('./WABridge')
 
 module.exports = async function () {
-  const thisModule = await require('./MyMoneroClient_WASM.js')({})
+  const thisModule = await require('./BeldexClient_WASM.js')({})
   return new WABridge(thisModule)
 }
